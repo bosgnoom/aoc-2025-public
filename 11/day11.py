94b4cc6c827cf56f10c6e80422ae70ffb8f6af59
Paul Schouten <paul.schouten@gmail.com>
2025-12-14 09:50:30 +0100

    Update on day 11

--- 11/day11.py
+++ 11/day11.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 import logging
 import coloredlogs
@@ -7,6 +8,8 @@ import matplotlib.pyplot as plt
 
 coloredlogs.install(level='INFO')
 logger = logging.getLogger(__name__)
+
+PLOTTING = True
 
 
 def read_input(filename: Path) -> dict:
@@ -28,48 +31,148 @@ def read_input(filename: Path) -> dict:
 
     # Turn the puzzle into a dict, with the first one as key
     data = {x[0]: list(x[1:]) for x in puzzle}
-    # logger.debug(pprint.pformat(puzzle))
+
     logger.debug(pprint.pformat(data))
 
     return data
 
 
-if __name__ == "__main__":
-    data = read_input(Path('11/input.txt'))
+def prune_graph(G: nx.DiGraph, source: str, dest: str) -> nx.DiGraph:
+    """
+    Cuts away all nodes before source and all nodes after target,
+    hereby reducing the network. This makes part 2 solvable, because the
+    network is soo large
+
+    :param G: Original network
+    :type G: nx.DiGraph
+    :param source: source
+    :type source: str
+    :param dest: destiny node
+    :type dest: str
+    :return: Description
+    :rtype: DiGraph
+    """
+
+    descendants = nx.descendants(G, source) | {source}  # add source back to descendants
+    ancestors = nx.ancestors(G, dest) | {dest}  # add destiny back to ancestors
+    subgraph = descendants & ancestors  # intersection (both in A and B)
+
+    return G.subgraph(subgraph).copy()
+
+
+def count_pathways(data: dict, source: str, dest: str) -> int:
+    """
+    Docstring for count_pathways
+
+    :param data: Dict like network information {"a": ['b', 'c']}
+    :type data: dict
+    :param source: source node
+    :type source: str
+    :param dest: destiny node
+    :type dest: str
+    :return: amount of different paths from source to destiny
+    :rtype: int
+    """
+
+    # Set up network
+    logger.info('Setting up network')
     G = nx.DiGraph(data)
+    logger.info(f'Original network: {G}')
 
-    print(G)
+    G = prune_graph(G, source, dest)
+    logger.info(f'Reduced network: {G}')
 
-    # Layout for positioning nodes
-    # pos = nx.spring_layout(G, seed=42, k=0.8)  # or try nx.kamada_kawai_layout(G)
+    if PLOTTING:
+        # Prepare figure
+        plt.figure(figsize=(16, 9))
 
-    # Draw nodes and edges
-    # plt.figure(figsize=(8, 6))
-    # nx.draw_networkx_nodes(G, pos, node_color="lightblue", node_size=1200, edgecolors="black")
-    # nx.draw_networkx_edges(G, pos, arrows=True, arrowstyle="-|>", arrowsize=25)
+        # Let's highlight "you" and "out"
+        highlight_nodes = [source, dest,  'you', 'out']
 
-    # Draw node labels (keys of the dict)
-    # nx.draw_networkx_labels(G, pos, font_size=10, font_color="black")
+        node_colors = [
+            'red' if node in highlight_nodes else 'lightblue'
+            for node in G.nodes()
+        ]
 
-    # plt.title("Directed Graph from Dictionary", fontsize=14)
-    # plt.axis("off")
-    # plt.tight_layout()
+        # Layout for positioning nodes
+        logger.info('Drawing network')
+        pos = nx.kamada_kawai_layout(G)
 
-    # paths = list(nx.all_simple_paths(G, source="you", target="out"))
-    # # pprint.pprint(paths)
-    # print(len(paths))
+        nx.draw_networkx_nodes(G, pos, node_size=100, alpha=0.8, node_color=node_colors)
+        nx.draw_networkx_edges(G, pos)
+        nx.draw_networkx_labels(G, pos, font_size=8)
 
-    # plt.show()
+    # Count how many paths lead from you to out, hurray for networkx
+    logger.info('Setting up all_simple_paths')
+    paths = nx.all_simple_paths(G, source=source, target=dest)  # this is a generator
 
-    # part 2
+    # Count amount of paths, show progress
+    logger.info('Counting all simple paths')
+    total = 0
+    for path in paths:
+        logger.debug(path)
 
-    p1 = nx.shortest_path(G, "svr", "fft")
-    p2 = nx.shortest_path(G, "fft", "dac")
-    p3 = nx.shortest_path(G, "dac", "out")
+        total += 1
 
-    print(p1)
-    print(p2)
-    print(p3)
+        if total % 10_000 == 0:
+            logger.info(f'\033[1m{source}\033[0m to \033[1m{dest}\033[0m: \033[1;91m{total}\033[0m')
 
-    p1 = list(nx.all_simple_paths(G, "svr", "fft"))
-    print(p1, len(p1))
+    logger.info(f'Amount of paths from \033[1m{source}\033[0m to \033[1m{dest}\033[0m: \033[1;91m{total}\033[0m')
+
+    return total
+
+
+def run_all(filename_part1: str, filename_part2: str) -> tuple[int, int]:
+    """
+    Runs both part 1 and part 2 of day 11,
+    Calculating amount of paths between sources and targets
+
+    :param filename_part1: Input part 1
+    :type filename_part1: str
+    :param filename_part2: Input part 2
+    :type filename_part2: str
+    :return: Amount of paths for part 1 and part 2
+    :rtype: tuple[int, int]
+    """
+    # Part 1
+    result_1 = count_pathways(read_input(Path(filename_part1)), "you", "out")
+
+    # Part 2
+    # Has different sample data
+    data = read_input(Path(filename_part2))
+
+    # It's going to be either svr->dac->fft->out or
+    #                         svr->fft->dac->out
+    # Let's find these routes
+    answer_part2 = []
+    try:
+        answer_part2.extend([count_pathways(data, "fft", "dac"),
+                            count_pathways(data, "svr", "fft"),
+                            count_pathways(data, "dac", "out"),
+                             ])
+
+    except nx.NodeNotFound:
+        logger.critical('fft->dac does not exist!')
+        answer_part2.extend([count_pathways(data, "dac", "fft"),
+                            count_pathways(data, "svr", "dac"),
+                            count_pathways(data, "fft", "out"),
+                             ])
+
+    except Exception as e:
+        logger.critical('Something went wrong:')
+        logger.critical(e)
+
+    # answer_part 2 has now thee items of possibilities,
+    # to get to total possibilities, multiply
+    result_2 = math.prod(answer_part2)
+    logger.info(f'Amount of routes from \033[1msvr\033[0m to \033[1mout\033[0m: \033[1;91m{result_2}\033[0m')
+
+    # Show figures
+    plt.show()
+
+    return result_1, result_2
+
+
+if __name__ == "__main__":  # pragma: no cover
+    # day11('11/sample01.txt', '11/sample02.txt')
+    run_all('11/input.txt', '11/input.txt')
